f67d795e106c26a437a56d6cd2fe616cad65a34b
Jose Ferreira <josebazjunior09@gmail.com>
2024-02-15 16:41:36 -0300

    [feat]: add initial logic to generate summary pdf

--- api/controllers/event.py
+++ api/controllers/event.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Depends, Query, status
+from fastapi.responses import FileResponse
 from api.adapters.repository.event import EventAdapter
+from api.adapters.repository.paper import PaperAdapter
 from api.models.dto.event import EventDTO
 from api.models.responses.event import EventResponse
 from api.services.event import EventService, EventsPaginatedResponse
 
 router = APIRouter()
 
-adapter = EventAdapter()
-service = EventService(adapter)
+event_adapter = EventAdapter()
+paper_adapter = PaperAdapter()
+service = EventService(event_adapter, paper_adapter)
 
 
 @router.post("", response_model=EventResponse, status_code=status.HTTP_201_CREATED)
@@ -15,6 +18,20 @@ def create_event(
     event_data: EventDTO, event_service: EventService = Depends(lambda: service)
 ):
     return event_service.create_event(event_data)
+
+
+@router.post(
+    "/{event_id}/summary",
+    status_code=status.HTTP_201_CREATED,
+)
+def create_summary(
+    event_id: int, event_service: EventService = Depends(lambda: service)
+):
+    file_content = event_service.create_summary(event_id)
+    with open("summary.pdf", "wb") as file:
+        file.write(file_content)
+
+    return FileResponse("summary.pdf", filename="summary.pdf")
 
 
 @router.get("", response_model=EventsPaginatedResponse, status_code=status.HTTP_200_OK)
--- api/services/event.py
+++ api/services/event.py
@@ -1,8 +1,12 @@
+from io import BytesIO
 from math import ceil
 from pydantic import BaseModel
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 from api.models.dto.event import EventDTO
 from api.models.responses.event import EventResponse
 from api.ports.event import EventRepository
+from api.ports.paper import PaperRepository
 
 
 class EventsPaginatedResponse(BaseModel):
@@ -13,13 +17,49 @@ class EventsPaginatedResponse(BaseModel):
 
 
 class EventService:
-    def __init__(self, event_repo: EventRepository):
+    def __init__(self, event_repo: EventRepository, paper_repo: PaperRepository):
         self._event_repo = event_repo
+        self._paper_repo = paper_repo
 
     def create_event(self, event: EventDTO) -> EventResponse:
         event_data = self._event_repo.create_event(event)
 
         return EventResponse.from_event(event_data)
+
+    def create_summary(self, event_id: int) -> bytes:
+        event_areas = self._paper_repo.get_areas_by_event_id(event_id)
+
+        buffer = BytesIO()
+        summary_pdf = canvas.Canvas(buffer, pagesize=letter)
+        summary_pdf.setFont("Helvetica-Bold", 12)
+
+        y_position = 750  # Posição inicial Y
+
+        for area in event_areas:
+            # Escreva o título da área
+            summary_pdf.setFont("Helvetica-Bold", 16)
+            summary_pdf.drawString(100, y_position, f"Área: {area}")
+            y_position -= 20  # Atualize a posição Y
+
+            papers = self._paper_repo.get_papers_by_area(area)
+            for paper in papers:
+                # Escreva o título do paper
+                summary_pdf.setFont("Helvetica-Bold", 12)
+                summary_pdf.drawString(165, y_position, str(paper.title))
+                y_position -= 20  # Atualize a posição Y
+
+                # Escreva os autores
+                summary_pdf.setFont("Helvetica", 12)
+                summary_pdf.drawString(165, y_position, f"Autores: {', '.join(paper.authors)}")
+                y_position -= 20  # Atualize a posição Y
+
+                if y_position < 50:
+                    summary_pdf.showPage()
+                    y_position = 750  # Reinicie a posição Y para a próxima página
+
+        summary_pdf.save()
+
+        return buffer.getvalue()
 
     def get_events(self, page: int = 1, page_size: int = 10) -> EventsPaginatedResponse:
         events_data = self._event_repo.get_events(page, page_size)
