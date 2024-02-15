104b81b9886f0f58f439d797b16ce2e590cd8e55
Jose Ferreira <josebazjunior09@gmail.com>
2024-02-15 20:10:12 -0300

    [feat]: add folder to key on s3 put object

--- api/adapters/aws/file_handler.py
+++ api/adapters/aws/file_handler.py
@@ -9,10 +9,12 @@ class FileHandlerS3Adapter(FileHandlerProvider):
         self._session = S3Config()
         self._bucket_name = settings.AWS_S3_BUCKET_NAME
 
-    def put_object(self, file_to_upload: bytes, key_obj: str):
+    def put_object(self, file_to_upload: bytes, folder: str, key_obj: str):
         try:
             return self._session.s3_client.put_object(
-                Body=file_to_upload, Bucket=self._bucket_name, Key=key_obj
+                Body=file_to_upload,
+                Bucket=self._bucket_name,
+                Key=folder + "/" + key_obj,
             )
         except ClientError as e:
             raise RuntimeError(
--- api/ports/file_handler.py
+++ api/ports/file_handler.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class FileHandlerProvider(ABC):
     @abstractmethod
-    def put_object(self, file_to_upload: bytes, key_obj: str):
+    def put_object(self, file_to_upload: bytes, folder: str, key_obj: str):
         ...
