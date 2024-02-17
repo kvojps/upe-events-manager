e0f596a4b9087573216c093e3a51ca16a3f5f80c
Jose Ferreira <josebazjunior09@gmail.com>
2024-02-17 02:45:09 -0300

    [feat]: add final merge pdf files logic

--- api/services/paper_pack.py
+++ api/services/paper_pack.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 import zipfile
 from fastapi import File, HTTPException, UploadFile, status
-from PyPDF2 import PdfMerger
+from PyPDF2 import PdfReader, PdfWriter
 from api.ports.event import EventRepository
 from api.services.file_handler import FileHandlerService, PutObjectResponse
 
@@ -19,7 +19,13 @@ class PaperPackService:
     async def merge_pdf_files(
         self, event_id: int, file: UploadFile = File(...)
     ) -> PutObjectResponse:
-        if file.filename and not file.filename.lower().endswith(".zip"):
+        if not file.filename:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="The file must have a name",
+            )
+
+        if not file.filename.lower().endswith(".zip"):
             raise HTTPException(
                 status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
                 detail="The file must be a zip file",
@@ -31,26 +37,32 @@ class PaperPackService:
 
         try:
             with tempfile.TemporaryDirectory() as temp_dir:
-                zip_file_path = os.path.join(temp_dir, file.filename)  # type: ignore
+                zip_file_path = os.path.join(temp_dir, file.filename)
                 with open(zip_file_path, "wb") as buffer:
                     buffer.write(await file.read())
 
-                merger = PdfMerger(False, "merged_papers.pdf")
-
+                pdf_writer = PdfWriter()
                 with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
                     for filename in zip_ref.namelist():
                         if filename.lower().endswith(".pdf"):
+                            zip_ref.extract(filename, path=temp_dir)
+                            pdf_reader = PdfReader(os.path.join(temp_dir, filename))
+                            for page_num in range(0, len(pdf_reader.pages)):
+                                page_obj = pdf_reader.pages[page_num]
+                                pdf_writer.add_page(page_obj)
+
                             with zip_ref.open(filename) as pdf_file:
-                                # self._file_handler_service.put_object(
-                                #     pdf_file.read(),
-                                #     f"{s3_folder_name}",
-                                #     filename,
-                                # )
-                                merger.append(pdf_file)
+                                self._file_handler_service.put_object(
+                                    pdf_file.read(),
+                                    f"{s3_folder_name}",
+                                    filename,
+                                )
+
                 merged_papers_path = os.path.join(temp_dir, "merged_papers.pdf")
                 with open(merged_papers_path, "wb+") as output_file:
-                    merger.write(output_file)
-                    merger.close()
+                    pdf_writer.write(merged_papers_path)
+                    pdf_writer.close()
+
                     put_object_response = self._file_handler_service.put_object(
                         output_file.read(),
                         s3_folder_name,
