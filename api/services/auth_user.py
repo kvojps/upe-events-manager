48a56aec27ff9f7bb095af44345f1d20d58122ff
Jose Ferreira <josebazjunior09@gmail.com>
2024-02-22 13:49:59 -0300

    [refac]: change method and variable names

--- api/services/auth_user.py
+++ api/services/auth_user.py
@@ -1,73 +1,54 @@
 from datetime import datetime, timedelta
-from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException
-from jose import JWTError, jwt
+from jose import jwt
 from passlib.context import CryptContext
-
+from sqlalchemy.exc import IntegrityError
 from api.config.dynaconf import settings
-from api.models.dto.user import UserDTO, UserLoginDTO
-from api.models.responses.user import UserResponse, UserLoginResponse
+from api.models.dto.user import AuthDTO, UserDTO
+from api.models.responses.user import AuthResponse, UserResponse
 from api.models.user import User
 from api.ports.user import UserRepository
 
-crypt_context = CryptContext(schemes=["sha256_crypt"])
+CRYPT_CONTEXT = CryptContext(schemes=["sha256_crypt"])
 
 
 class AuthService:
     def __init__(self, user_repo: UserRepository):
         self._user_repo = user_repo
-        self._JWT_SECRET_KEY = settings.JWT_SECRET_KEY
-        self._JWT_ALGORITHM = settings.JWT_ALGORITHM
+        self._jwt_secret_key = settings.JWT_SECRET_KEY
+        self._jwt_algorithm = settings.JWT_ALGORITHM
 
-    def register_user(self, user_request: UserDTO) -> UserResponse:
-        user = User.from_dto(user_request)
+    def create_user(self, user: UserDTO) -> UserResponse:
+        user_data = User.from_dto(user)
 
         try:
-            user_created = self._user_repo.create_user(user)
-            return UserResponse.from_user(user_created)
-
+            return UserResponse.from_user(self._user_repo.create_user(user_data))
         except IntegrityError:
             raise HTTPException(status_code=400, detail="User already exists")
 
-    def user_login(self, user_request: UserLoginDTO) -> UserLoginResponse:
-        user_db = self._user_repo.get_user_by_username(user_request.username)
+    def authenticate_user(self, auth: AuthDTO) -> AuthResponse:
+        user_data = self._user_repo.get_user_by_username(auth.username)
 
-        if user_db is None:
+        if user_data is None:
             raise HTTPException(
                 status_code=401, detail="Username or password incorrect"
             )
 
-        if not crypt_context.verify(user_request.password, user_db.password):  # type: ignore
+        if not CRYPT_CONTEXT.verify(auth.password, str(user_data.password)):
             raise HTTPException(
                 status_code=401, detail="Username or password incorrect"
             )
 
-        exp = datetime.utcnow() + timedelta(minutes=(30))
+        expiration_time = datetime.utcnow() + timedelta(days=1)
 
-        payload = {"sub": user_db.username, "exp": exp}
+        payload = {"sub": user_data.username, "exp": expiration_time}
 
         access_token = jwt.encode(
-            payload, self._JWT_SECRET_KEY, algorithm=self._JWT_ALGORITHM
+            payload, self._jwt_secret_key, algorithm=self._jwt_algorithm
         )
 
-        return UserLoginResponse(
+        return AuthResponse(
             access_token=access_token,
-            expires_in=exp,
-            user=UserResponse.from_user(user_db),
+            expires_in=expiration_time,
+            user=UserResponse.from_user(user_data),
         )
-
-    def verify_token(self, token: str):
-        try:
-            payload = jwt.decode(
-                token, self._JWT_SECRET_KEY, algorithms=[self._JWT_ALGORITHM]
-            )
-
-        except JWTError:
-            raise HTTPException(status_code=401, detail="Invalid token")
-
-        user_db = self._user_repo.get_user_by_username(payload["sub"])
-
-        if user_db is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
-
-        return user_db
