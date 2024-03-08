846a80dda3154a6446b9cecafadfc5e631edaf3b
kvojps <josebazjunior09@gmail.com>
2024-03-07 22:57:15 -0300

    [feat]: add cors

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.config.postgres import init_postgres_db
 from api.controllers import main_router
-from api.models import create_tables, init_config_db
+from api.models import init_config_db
 
 
 @asynccontextmanager
@@ -18,6 +19,14 @@ app = FastAPI(
     title="API - UPE Events Management System",
     version="0.1.0",
     description="Sistema de gerenciamento de eventos da UPE - Campus Garanhuns.",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(main_router)
