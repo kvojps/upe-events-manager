096f24dcb80d37e7f868bcddecb96600b61e8a25
gabriel rodrigues <gabriel.rtlima@upe.br>
2024-05-09 18:10:10 -0300

    feat: add logic to send certificates and fix certificate pdf

--- api/services/subscriber.py
+++ api/services/subscriber.py
@@ -11,6 +11,7 @@ from validate_docbr import CPF  # type: ignore
 from api.config.dynaconf import settings
 from api.models.event import Event
 from api.models.subscriber import Subscriber
+from api.ports.email_handler import EmailHandlerProvider
 from api.ports.event import EventRepository
 from api.ports.subscriber import SubscriberRepository
 from api.services.responses.subscriber import (
@@ -23,10 +24,14 @@ from api.utils.user_validator import validate_cpf, validate_email
 
 class SubscriberService:
     def __init__(
-        self, subscriber_repo: SubscriberRepository, event_repo: EventRepository
+        self,
+        subscriber_repo: SubscriberRepository,
+        event_repo: EventRepository,
+        ses_adapter: EmailHandlerProvider,
     ):
         self._subscriber_repo = subscriber_repo
         self._event_repo = event_repo
+        self._ses_adapter = ses_adapter
 
     async def batch_create_subscribers(
         self, event_id: int, file: UploadFile = File(...)
@@ -154,8 +159,25 @@ class SubscriberService:
         html_bytes_certificate = self._get_certificate_html(event, subscriber)
         return self._get_certficate_pdf(html_bytes_certificate)
 
+    async def send_all_certificates(self, event_id: int):
+        listeners = self._subscriber_repo.get_listeners(event_id)
+
+        if listeners is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Listeners not found",
+            )
+
+        for listener in listeners:
+            certificate = self.get_subscriber_certificate(event_id, str(listener.email))
+            self._ses_adapter.send_raw_email(certificate, listener)
+            print(listener.email)
+
     def _get_certificate_html(self, event: Event, subscriber: Subscriber):
         buffer = BytesIO()
+
+        initial_date = str(event.initial_date).replace("-", "/")
+        final_date = str(event.final_date).replace("-", "/")
 
         template_html = """
             <!DOCTYPE html>
@@ -164,17 +186,12 @@ class SubscriberService:
                 <title>Certificado SECAP</title>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="preconnect" href="https://fonts.googleapis.com">
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-                    rel="stylesheet">
                 <style>
                     body {
-                        margin: 0;
+                        margin: 0px;
                     }
                     main {
-                        font-family: Poppins;
+                        font-family: Arial;
                     }
                     header {
                         background-color: #050973;
@@ -185,7 +202,8 @@ class SubscriberService:
                         padding-top: 84px;
                     }
                     p {
-                        margin: 0;
+                        margin: 0px;
+                        font-size: 24px;
                     }
                     .div-titulo {
                         height: 200px;
@@ -243,9 +261,9 @@ class SubscriberService:
                     </div>
                     <div class="div-desc">
                         <p>
-                            CERTIFICAMOS QUE <strong> {subscriber.name}</strong> PARTICIPOU COMO <strong>OUVINTE</strong> NA
-                            <strong> {event.name}</strong>,
-                            PROMOVIDA PELA {event.promoted_by}, DE {event.initial_date} A {event.final_date}, COM CARGA HORÁRIA DE
+                            CERTIFICAMOS QUE <strong> {str(subscriber.name).upper()}</strong> PARTICIPOU COMO <strong>OUVINTE</strong> NA
+                            <strong> {str(event.name).upper()}</strong>,
+                            PROMOVIDA POR {str(event.promoted_by).upper()}, DE <strong>{initial_date}</strong> A <strong>{final_date}</strong>, COM CARGA HORÁRIA DE
                             <strong>{subscriber.workload} </strong>HORAS.
                         </p>
 
@@ -297,6 +315,12 @@ class SubscriberService:
         temp_pdf_file_path = f"{temp_dir}/{session_id}__summary.pdf"
         options = {
             "orientation": "Landscape",
+            "page-size": "A4",
+            "no-outline": None,
+            "margin-top": "0",
+            "margin-right": "0",
+            "margin-bottom": "0",
+            "margin-left": "0",
         }
         pdfkit.from_file(temp_html_file_path, temp_pdf_file_path, options=options)
 
